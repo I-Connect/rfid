2d844aefc71c418a364f340e6b9fc3835c8a7bc9
Jeroen <Jeroen@i-connect.online>
2022-08-17 15:49:41 +0200

    changed hwserial to stream

--- src/MFRC522_uart.cpp
+++ src/MFRC522_uart.cpp
@@ -9,11 +9,10 @@
 #include <Arduino.h>
 #include <MFRC522.h>
 
-MFRC522_UART::MFRC522_UART(HardwareSerial &serial, byte resetPin)
-	: _resetPowerDownPin(resetPin), _serialPort(serial) {}
+MFRC522_UART::MFRC522_UART(Stream& serial, byte resetPin)
+  : _resetPowerDownPin(resetPin), _serialPort(serial) {}
 
 bool MFRC522_UART::PCD_Init() {
-  _serialPort.begin(9600, SERIAL_8N1);
 
   if (_resetPowerDownPin != UNUSED_PIN) {
     pinMode(_resetPowerDownPin, OUTPUT);
@@ -27,49 +26,50 @@ bool MFRC522_UART::PCD_Init() {
 }
 
 void MFRC522_UART::PCD_WriteRegister(MFRC522::PCD_Register reg, byte value) {
-	// Mask used for writing to registers. See section "8.1.3.3 UART framing" from datasheet.
-	_serialPort.write(0x7F & reg);
-	// Waits for confirmation
-  	while (!_serialPort.available());
-	// Removes confirmation byte from input buffer
-  	_serialPort.read();
-	// Writes data
-	_serialPort.write(value);
+  // Mask used for writing to registers. See section "8.1.3.3 UART framing" from datasheet.
+  _serialPort.write(0x7F & reg);
+  // Waits for confirmation
+  while (!_serialPort.available());
+  // Removes confirmation byte from input buffer
+  _serialPort.read();
+  // Writes data
+  _serialPort.write(value);
 }
 
 void MFRC522_UART::PCD_WriteRegister(
-  	MFRC522::PCD_Register reg, byte count, byte *values) {
+  MFRC522::PCD_Register reg, byte count, byte* values) {
 
-	for (byte index = 0; index < count; ++index)
-    	this->PCD_WriteRegister(reg, values[index]);
+  for (byte index = 0; index < count; ++index) {
+    this->PCD_WriteRegister(reg, values[index]);
+  }
 }
 
 byte MFRC522_UART::PCD_ReadRegister(MFRC522::PCD_Register reg) {
-	// Mask used for reading from registers. See section "8.1.3.3 UART framing" from datasheet.
-	_serialPort.write(0x80 | reg);
-	// Waits for input
-	while (!_serialPort.available());
-	// Reads data
-	byte value = _serialPort.read();
-	return value;
+  // Mask used for reading from registers. See section "8.1.3.3 UART framing" from datasheet.
+  _serialPort.write(0x80 | reg);
+  // Waits for input
+  while (!_serialPort.available());
+  // Reads data
+  byte value = _serialPort.read();
+  return value;
 }
 
 void MFRC522_UART::PCD_ReadRegister(
-  	MFRC522::PCD_Register reg, byte count, byte *values, byte rxAlign) {
+  MFRC522::PCD_Register reg, byte count, byte* values, byte rxAlign) {
 
-	byte index = 0;
-	// Only update bit positions rxAlign..7 in values[0]
-	if (rxAlign) {
-		byte mask = 0xFF << rxAlign;
-		byte value = this->PCD_ReadRegister(reg);
-		// Apply mask to both current value of values[0] and the new data in value.
-		values[0] = (values[0] & ~mask) | (value & mask);
-		++index;
-	}
+  byte index = 0;
+  // Only update bit positions rxAlign..7 in values[0]
+  if (rxAlign) {
+    byte mask = 0xFF << rxAlign;
+    byte value = this->PCD_ReadRegister(reg);
+    // Apply mask to both current value of values[0] and the new data in value.
+    values[0] = (values[0] & ~mask) | (value & mask);
+    ++index;
+  }
 
-	while (index < count) {
-		// Keeps reading from the same address
-		values[index] = this->PCD_ReadRegister(reg);
-		++index;
-	}
+  while (index < count) {
+    // Keeps reading from the same address
+    values[index] = this->PCD_ReadRegister(reg);
+    ++index;
+  }
 }
